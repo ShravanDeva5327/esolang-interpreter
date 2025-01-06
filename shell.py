dd9df678034cfbf8b435d48d8d429a149fa91784
Shravan Deva <noobcoder5327@gmail.com>
2025-01-06 12:54:50 +0530

    add functionality to run a fie

--- shell.py
+++ shell.py
@@ -1,33 +1,66 @@
 from lexer import *
+import argparse
 
-variables = {}
-while True:
+arg_parser = argparse.ArgumentParser(description='')
+arg_parser.add_argument('file', nargs='?', default=None, help='File to run')
+args = arg_parser.parse_args()
+
+if args.file is None:
+    variables = {}
+    while True:
+        try:
+            text = input(">>> ")
+
+            # Generate tokens
+            lexer_ = Lexer(text)
+            tokens = lexer_.tokenize()
+
+            if tokens is not None:
+                # print(tokens)
+
+                # Generate AST
+                parser_ = Parser(text, tokens)
+                ast_list = parser_.parse()
+                if ast_list is not None:
+                    # print(ast_list)
+                    for ast in ast_list:
+                        interpreter_ = Interpreter(text, ast, variables)
+                        result = interpreter_.interpret()
+                        # if result is not None:
+                            # print(f'{result.value}: {result.type}')
+        
+        except EOFError:
+            print("")
+            break
+        except KeyboardInterrupt:
+            print("")
+            continue
+        except Exception as e:
+            print(e)
+
+else:
     try:
-        text = input(">>> ")
+        with open(args.file, 'r') as f:
+            variables = {}
+            text = f.read()
 
-        # Generate tokens
-        lexer_ = Lexer(text)
-        tokens = lexer_.tokenize()
+            # Generate tokens
+            lexer_ = Lexer(text)
+            tokens = lexer_.tokenize()
 
-        if tokens is not None:
-            # print(tokens)
+            if tokens is not None:
+                # print(tokens)
 
-            # Generate AST
-            parser_ = Parser(text, tokens)
-            ast_list = parser_.parse()
-            if ast_list is not None:
-                # print(ast_list)
-                for ast in ast_list:
-                    interpreter_ = Interpreter(text, ast, variables)
-                    result = interpreter_.interpret()
-                    # if result is not None:
-                        # print(f'{result.value}: {result.type}')
-    
-    except EOFError:
-        print("")
-        break
-    except KeyboardInterrupt:
-        print("")
-        continue
-    # except Exception as e:
-    #     print(e)
+                # Generate AST
+                parser_ = Parser(text, tokens)
+                ast_list = parser_.parse()
+                if ast_list is not None:
+                    # print(ast_list)
+                    for ast in ast_list:
+                        interpreter_ = Interpreter(text, ast, variables)
+                        result = interpreter_.interpret()
+                        # if result is not None:
+                            # print(f'{result.value}: {result.type}')
+
+    except Exception as e:
+        print(e)
